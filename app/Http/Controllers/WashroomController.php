3532c99c51844183891e37384f1f9363a82e3633
marc limson <marc.limson@icopylegal.com>
2025-09-01 16:21:13 +0800

    initital commit

---
+++ app/Http/Controllers/WashroomController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WashroomController extends Controller
+{
+    public function create() 
+    {
+
+    }
+
+    public function list()
+    {
+        return "list";
+    }
+}
