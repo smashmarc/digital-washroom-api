ebe31dd590aaa4a141629ef465554985c097c8c5
marc limson <marc.limson@icopylegal.com>
2026-04-25 22:54:06 +0800

    fix mysqldump 2

--- app/Services/BackupService.php
+++ app/Services/BackupService.php
@@ -163,7 +163,7 @@ class BackupService
         if ($driver === 'pgsql') {
             $inner = "PGPASSWORD={$escapedPass} pg_dump -h {$host} -p {$port} -U {$user} {$typeFlag} {$dbname} | gzip > {$outPath}";
         } else {
-            $inner = "mysqldump -h {$host} -P {$port} -u {$user} -p{$escapedPass} --ssl=0 {$typeFlag} {$dbname} | gzip > {$outPath}";
+            $inner = "mysqldump -h {$host} -P {$port} -u {$user} -p{$escapedPass} --skip-ssl {$typeFlag} {$dbname} | gzip > {$outPath}";
         }
 
         // pipefail ensures we get mysqldump's exit code, not gzip's
