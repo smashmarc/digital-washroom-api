9be1b276c76499901fc6f7d871891619a5a5c5f5
marc limson <marc.limson@icopylegal.com>
2026-06-09 15:49:17 +0800

    added link to needs attention

--- routes/api.php
+++ routes/api.php
@@ -60,6 +60,7 @@ Route::middleware('auth:api')->group(function () {
     });
 
     Route::get('dashboard', [DashboardController::class, 'index']);
+    Route::get('dashboard/needs-attention', [DashboardController::class, 'needsAttention']);
 
     Route::post('force-change-password', [AuthController::class, 'forceChangePassword']);
 
